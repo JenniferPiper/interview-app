043bb30d0c554ce3c6cf1dc005708b34f1a9b0b4
Jennifer Piper <jennifer@squareandplum.com>
2018-08-22 14:48:13 -0700

    set up scss, add timer button and starter button styles

---
+++ src/components/timer/timer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './timer.css';
+
+
+class Timer extends React.Component {
+    public render() {
+        return (
+            <section className="Timer">
+            <button className="Timer-button">Start Challenge</button>
+            </section>
+        );
+    }
+}
+
+export default Timer;
