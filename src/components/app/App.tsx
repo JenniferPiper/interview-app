08d66a3a6aafe4299c0099fb4afff4f452200aaf
Jennifer Piper <jennifer@squareandplum.com>
2018-08-22 12:38:42 -0700

    organize folders, add placeholder title and tagline

---
+++ src/components/app/App.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import './App.css';
+
+import logo from '../../assets/logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">CrushIt</h1>
+            <h2 className="App-tagline">Code Challenge Companion <span>(prototype)</span></h2>
+        </header>
+        {/*<p className="App-intro">*/}
+          {/*To get started, edit <code>src/App.tsx</code> and save to reload.*/}
+        {/*</p>*/}
+      </div>
+    );
+  }
+}
+
+export default App;
