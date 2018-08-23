7ef0dc8884d70ff9b9b8d871ed135df91fc8cffa
Jennifer Piper <jennifer@squareandplum.com>
2018-08-22 17:54:30 -0700

    set up constructor and JSX for Timer component

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import './timer.css';
 
+interface ITimerState{ timerDisplay: boolean}
+class Timer extends React.Component<any, ITimerState> {
+    constructor(props: any) {
+        super(props);
 
-class Timer extends React.Component {
+        this.state = {
+            timerDisplay: false
+        }
+    }
     public render() {
+        const startButtonJSX = <div>
+            <button className="Timer-button">Start Challenge</button>
+        </div>;
+        const timerJSX = <div>
+            <p>Timer goes here.</p>
+        </div>;
         return (
             <section className="Timer">
-            <button className="Timer-button">Start Challenge</button>
+                {this.state.timerDisplay ? timerJSX : startButtonJSX}
+                {/*{startButtonJSX}*/}
+                {/*{timerJSX}*/}
+
             </section>
         );
     }
