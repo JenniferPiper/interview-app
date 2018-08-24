4869dbe907bed6ccec852783718aecbc70760e30
Jennifer Piper <jennifer@squareandplum.com>
2018-08-23 17:26:27 -0700

    implement very basic countdown timer

--- src/components/timer/timer.tsx
+++ src/components/timer/timer.tsx
@@ -1,20 +1,36 @@
+// tslint:disable:no-console
 import * as React from 'react';
 
 import autoBind from '../../utils/utils.js'
 import './timer.css';
 
-interface ITimerState{ timerDisplay: boolean}
+interface ITimerState{ timerDisplay: boolean, timeRemaining: number, clock: any}
 class Timer extends React.Component<any, ITimerState> {
     constructor(props: any) {
         super(props);
 
         this.state = {
+            clock: null,
+            timeRemaining: 4,
             timerDisplay: false
+
+
         };
         autoBind.call(this, Timer);
     }
     public handleStart() {
         this.setState({timerDisplay: true});
+        const clockInstance = setInterval(this.handleTimeDecrement, 1000);
+        this.setState({ clock: clockInstance });
+    }
+    public handleTimeDecrement() {
+        if (this.state.timeRemaining > 0) {
+            this.setState({timeRemaining: this.state.timeRemaining - 1});
+        }
+        else {
+            clearInterval(this.state.clock);
+        }
+        console.log('handleTimeDecrement');
     }
     public render() {
         const startButtonJSX = <div>
@@ -24,7 +40,9 @@ class Timer extends React.Component<any, ITimerState> {
                 Start Challenge</button>
         </div>;
         const timerJSX = <div>
-            <p>Timer goes here.</p>
+            <p>Time Remaining:</p>
+            <h2>{this.state.timeRemaining}</h2>
+
         </div>;
         return (
             <section className="Timer">
